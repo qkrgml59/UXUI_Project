ec493215a2e35f98c90ddca5ffbe3ba5e20ba3c8
qkrgml59 <qkrgml59@gmail.com>
2026-06-11 14:55:26 +0900

    [Feat] TilteScene 구현

---
+++ Assets/_PHY/Scripte/ButtonHoverEffect.cs
@@ -0,0 +1,29 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour
+{
+    [Header("호버 크기")]
+    [SerializeField] private float hoverScale = 1.1f;
+
+    [Header("애니메이션 시간")]
+    [SerializeField] private float duration = 0.15f;
+
+    private Vector3 originalScale;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(originalScale * hoverScale, duration);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(originalScale, duration);
+    }
+}
---
+++ Assets/_PHY/Scripte/TilteUI.cs
@@ -0,0 +1,50 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TilteUI : MonoBehaviour
+{
+    [Header("씬 이름")]
+    [SerializeField] private string mainSceneName = "MainScene";
+
+    [Header("페이드 이미지")]
+    [SerializeField] private Image fadeImage;
+
+    [Header("페이드 시간")]
+    [SerializeField] private float fadeDuration = 0.5f;
+
+    private bool isLoading;
+
+    private void Start()
+    {
+        Color color = fadeImage.color;
+        color.a = 1f;
+        fadeImage.color = color;
+
+        fadeImage.DOFade(0f, fadeDuration);
+    }
+
+    public void OnClickStart()
+    {
+        if (isLoading)
+            return;
+
+        isLoading = true;
+
+        fadeImage.DOFade(1f, fadeDuration)
+            .OnComplete(() =>
+            {
+                SceneManager.LoadScene(mainSceneName);
+            });
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+    }
+}
